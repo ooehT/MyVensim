5222fd0d4672f337b9b6612052266f3d195af768
Theo dos Anjos Silva <153386387+vVoide@users.noreply.github.com>
2026-05-17 16:08:53 -0300

    Minha mensagem de commit explicativa

---
+++ src/flow.hpp
@@ -0,0 +1,31 @@
+#ifndef FLOW_HPP
+#define FLOW_HPP
+
+#include <string>
+#include "system.hpp"
+
+class Flow {
+    protected:
+        string name;
+        System* input;
+        System* output;
+
+    public:
+        Flow();
+        Flow(string name, System* input, System* output);
+        Flow(const Flow &f);
+        Flow &operator=(const Flow &f); 
+        virtual ~Flow();
+        virtual double execute() = 0;
+        void setSource(System* input);
+        System* getSource() const;
+        void setTarget(System* output);
+        System* getTarget() const;
+        string getName() const;
+        void setName(const string &value);
+        void removeSource();
+        void removeTarget();
+
+};
+
+#endif
